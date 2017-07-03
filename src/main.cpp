94b1d9230d0705f8ca99824c6d1a8e04ea2c12ce
subhash <subhashgo@gmail.com>
2017-07-03 08:31:35 +0530

    More traction

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  pid.Init(0.15, 0.023/200.0, 1.6);
+  pid.Init(0.15, 0.023/200.0, 3.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
